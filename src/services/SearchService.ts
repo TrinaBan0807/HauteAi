b8e49c7bd58bd55554d5e8971c5eda6aaeb1b428
TrinaBan0807 <141120945+TrinaBan0807@users.noreply.github.com>
2025-06-06 13:42:00 +0530

    Update SearchService.ts

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -61,14 +61,14 @@ export class SearchService {
       'https://images.unsplash.com/photo-1582582621959-48d27397dc69?w=400&h=400&fit=crop&q=80'
     ],
     'handbag': [
-      'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQaRER-zHFxcS6bxoQoGKFiglX6aAjTzruCwZ_UzpBwAQ0j_QZem6wxl8vfEBQRMK6bBBmPFfF94uTEgm5U-Fq1B5Mz6V83IPdwYTqKsIawZwUZwQR4rJGTkg?w=400&h=400&fit=crop&q=80',
+      'https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?q=80&w=1963&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       'https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=400&h=400&fit=crop&q=80',
       'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=400&h=400&fit=crop&q=80'
     ],
     'bag': [
-      'https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?w=400&h=400&fit=crop&q=80',
+      'https://images.unsplash.com/photo-1622560480654-d96214fdc887?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJhZ3N8ZW58MHx8MHx8fDA%3D',
       'https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=400&h=400&fit=crop&q=80',
-      'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=400&h=400&fit=crop&q=80'
+      'https://images.unsplash.com/photo-1630830607408-261889eb4968?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fGJhZ3N8ZW58MHx8MHx8fDA%3D'
     ],
     'shoes': [
       'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400&h=400&fit=crop&q=80',
